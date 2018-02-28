38969de0d9d0b6576342277f10018cb11c3142db
Agata Drozdowska <Agata@Agatas-MacBook-Pro.local>
2018-02-27 20:01:26 -0600

    higherOrderFunction Project

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -3,15 +3,27 @@
 const assert = require('assert');
 
 function forEach(arr, callback) {
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    callback();
+  }
 }
 
 function map(arr, callback) {
-  // Your code here
+  let mapped = [];
+  for (let i = 0; i < arr.length; i++) {
+    mapped.push(callback(arr[i]));
+  }
+  return mapped;
 }
 
 function filter(arr, callback) {
-  // Your code here
+  let filtered = [];
+  for(let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      filtered.push(arr[i]);
+    }
+  }
+  return filtered;
 }
 
 function some(arr, callback) {
